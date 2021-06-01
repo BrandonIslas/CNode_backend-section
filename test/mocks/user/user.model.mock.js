f8b4c97673bfe52446e73324511f416a80a271d1
Brandon Islas <brandon98642@gmail.com>
2021-06-01 13:20:02 -0500

    [ADD] tests added

---
+++ test/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+  user: {
+    _id: "60b66ee21eb5894f004cc285",
+    name: "Tomas",
+    username: "Tomas",
+    password: "12345"
+  },
+  users: [
+    {
+      _id: "60b66ee21eb5894f004cc285",
+      name: "Tomas",
+      username: "Tomas",
+      password: "12345"
+    },
+    {
+      _id: "60b66f3e1eb5894f004cc288",
+      name: "Rodri",
+      username: "Rodri",
+      password: "12345"
+    },
+  ],
+};
